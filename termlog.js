2eac9d25eb803663780281d8705809d8f1b9fe8f
sergeyt <stodyshev@gmail.com>
2013-12-10 01:00:45 +0700

    public api

--- termlog.js
+++ termlog.js
@@ -1,5 +1,9 @@
 (function(){
 
+	if (typeof console === 'undefined'){
+		return;
+	}
+
 	var _term;
 
 	function term(){
@@ -15,37 +19,63 @@
 		return _term;
 	}
 
-	function toArray(args){
-		return [].slice.call(args);
-	}
+	function toArray(args){ return [].slice.call(args); }
+	// TODO simple fallback without sprintf
+	function format(args){ return sprintf.apply(sprintf, toArray(args)); }
 
-	function format(args){
-		return sprintf.apply(sprintf, toArray(args));
-	}
-
-	var colors = {
+	var levels = {
 		log: 32,
 		warn: 33,
-		error: 41
+		error: 41,
+		debug: 36
 	};
 
-	function color(msg, name){
-		var c = colors[name];
+	function colorize(msg, name){
+		var c = levels[name];
 		return '\x1b[' + c + 'm' + msg + '\x1b[m';
 	}
 
 	function wrap(name){
 		var fn = console[name];
-		console[name + '_'] = fn; // old function for restore
+		if (fn) console[name + '_'] = fn; // old function for restore
 		console[name] = function(){
-			fn.apply(console, toArray(arguments));
-			var msg = color(format(arguments), name);
+			if (fn) fn.apply(console, toArray(arguments));
+			var msg = colorize(format(arguments), name);
 			term().writeln(msg);
 		};
 	}
 
-	wrap('log');
-	wrap('warn');
-	wrap('error');
+	function unwrap(name){
+		var old = console[name + '_'];
+		if (!old) return;
+		console[name] = old;
+		delete console[name + '_'];
+	}
 
-}).call(this);
+	function open(){
+		Object.keys(levels).forEach(function(name){
+			wrap(name);
+		});
+	}
+
+	function close(){
+		Object.keys(levels).forEach(function(name){
+			unwrap(name);
+		});
+	}
+
+	// expose public api
+	this.terminal = {
+		open: open,
+		close: close
+	};
+
+	Object.keys(levels).forEach(function(name){
+		this.terminal[name] = function(){
+			console[name].apply(console, toArray(arguments));
+		};
+	});
+
+}).call((function() {
+	return this || (typeof window !== 'undefined' ? window : global);
+})());
